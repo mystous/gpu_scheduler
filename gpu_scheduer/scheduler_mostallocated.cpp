3cd807a94133be40a118a514c6d279ff43ba37b9
Kyunam Cho <mystous@naver.com>
2024-05-01 09:52:45 +0900

    Modifying Mostallocated scheduer a little bit

--- gpu_scheduer/scheduler_mostallocated.cpp
+++ gpu_scheduer/scheduler_mostallocated.cpp
@@ -6,6 +6,33 @@ int scheduler_mostallocated::arrange_server(job_entry& job) {
   int arrange_server = -1, i;
   int min_gap = max_value;
   vector<tuple<int, int, int>> candidate;
+  vector<tuple<int, server_entry*>> suitable_server_list;
+
+  get_suitable_server(suitable_server_list, job.get_accelerator_count());
+  for (auto& [index, server] : suitable_server_list) {
+    if (server->get_avaliable_accelator_count() < job.get_accelerator_count()) { continue; }
+
+    int gap = server->get_avaliable_accelator_count() - job.get_accelerator_count();
+    candidate.emplace_back(index, server->get_avaliable_accelator_count(), gap);
+
+    if (gap < min_gap) {
+      min_gap = gap;
+    }
+  }
+
+  for (auto& [index, server_accelator_count, gap] : candidate) {
+    if (min_gap != gap) {
+      continue;
+    }
+    arrange_server = index;
+    break;
+  }
+
+  if (-1 != arrange_server) {
+    server_entry* target = &target_server->at(arrange_server);
+    target->assign_accelator(&job, job.get_accelerator_count());
+    return arrange_server;
+  }
 
   for (i = 0; i < target_server->size(); ++i) {
     server_entry* target = &target_server->at(i);
@@ -13,28 +40,21 @@ int scheduler_mostallocated::arrange_server(job_entry& job) {
       continue;
     }
 
-    int gap = target->get_avaliable_accelator_count() - job.get_accelerator_count();
-    candidate.emplace_back(i, target->get_avaliable_accelator_count(), gap);
-
-    if (gap < min_gap) {
-      min_gap = gap;
-    }
+    arrange_server = i;
+    break;
   }
-
-  int min_accelator = max_value;
-  for (auto& [index, server_accelator_count, gap] : candidate) {
-    if (min_gap != gap) {
-      continue;
-    }
-    if (server_accelator_count < min_accelator) {
-      min_accelator = server_accelator_count;
-      arrange_server = index;
-    }
-  }
-
+    
   if (-1 != arrange_server) {
     server_entry* target = &target_server->at(arrange_server);
     target->assign_accelator(&job, job.get_accelerator_count());
   }
   return arrange_server;
+}
+
+void scheduler_mostallocated::get_suitable_server(vector<tuple<int, server_entry*>>& server, int required_accelerator){
+  // return most suitable accelerator group
+}
+
+void scheduler_mostallocated::postproessing_set_server() {
+  // Grouping Server with accelerator counts
 }
--- gpu_scheduer/scheduler_mostallocated.h
+++ gpu_scheduer/scheduler_mostallocated.h
@@ -5,6 +5,9 @@ public:
   virtual int arrange_server(job_entry& job) override;
 
 protected:
-  virtual void postproessing_set_server() override {};
+  virtual void postproessing_set_server() override;
+
+private:
+  void get_suitable_server(vector<tuple<int, server_entry*>> &server, int required_accelerator);
 };
 
